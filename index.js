1e13024176d90e1024be2903c18e7aca4747fd93
rahul1237 <rahulmahajan98645@gmail.com>
2021-03-02 13:12:44 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-let a='rahulmahajan'
-// console.log(a.length);
+var prompt = require('prompt')
+
+let a=prompt('enter a string')
 var len = a.length
 var x
 var part_a=''
 var part_b=''
-for(x=0;x<len/2;x++){
+for(x=0;x<Math.floor(len/2);x++){
     part_a+=a[x]
 }
-for(x=len/2;x<len;x++){
+for(x=Math.floor(len/2);x<len;x++){
     part_b+=a[x]
 }
 
