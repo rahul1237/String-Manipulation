7237bbf9f2c230e6b93e7c30444129fb50d41f12
rahul1237 <rahulmahajan98645@gmail.com>
2021-03-02 17:16:19 +0530

    second method added

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
-var prompt = require('prompt')
+// case 1
 
-let a=prompt('enter a string')
-var len = a.length
-var x
-var part_a=''
-var part_b=''
-for(x=0;x<Math.floor(len/2);x++){
-    part_a+=a[x]
-}
-for(x=Math.floor(len/2);x<len;x++){
-    part_b+=a[x]
+// let a='geeksforgeek'
+// var len = a.length
+// var x
+// var part_a=''
+// var part_b=''
+
+// // ceil or floor value :)
+// for(x=0;x<Math.ceil(len/2);x++){
+//     part_a+=a[x]
+// }
+// for(x=Math.ceil(len/2);x<len;x++){
+//     part_b+=a[x]
+// }
+
+// a=part_a.toUpperCase()+part_b
+// console.log(a);
+
+
+// case 2
+
+let a='rahula'
+var i = a.length
+b=''
+
+for(j=0;j<Math.floor(i/2);j++){
+    b+=a[j].toUpperCase()
 }
 
-a=part_a.toUpperCase()+part_b
-console.log(a);
+for(j=Math.floor(i/2);j<i;j++){
+    b+=a[j]
+}
+
+console.log(b);
